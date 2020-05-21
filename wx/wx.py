9ffc5c2f4dff3c838a92ca66503955da904f1ad6
Zszen John <zszen@hotmail.com>
2020-05-21 22:00:55 +0800

    by Zszen

---
+++ wx/wx.py
@@ -0,0 +1,14 @@
+# from wxpy import *
+
+# bot = Bot()
+
+# # my_friend = bot.friends().search('Z.', sex=FEMALE)[0]
+
+# # print(my_friend)
+
+
+import itchat
+
+itchat.auto_login()
+
+itchat.send('Hello, filehelper', toUserName='filehelper')
