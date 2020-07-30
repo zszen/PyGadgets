287387cede0cc887626092a8aafb65cb176affe5
zszen <zszen@hotmail.com>
2020-07-31 07:11:27 +0800

    By Zszen iMac

--- snoop/comic/manhuadb.com.py
+++ snoop/comic/manhuadb.com.py
@@ -4,10 +4,11 @@ from bs4 import BeautifulSoup
 import requests
 from lxml import etree
 import threading
+import time
 
 root = os.path.dirname(__file__)
 site = 'https://www.manhuadb.com/'
-comic_page = f'manhua/139/1328_13260%s.html'
+comic_page = f'manhua/139/1328_13264%s.html'
 
 
 def get_page(url, page=1):
@@ -28,8 +29,8 @@ def down(url, page=1):
 def parse_title(content):
     bs = BeautifulSoup(content, features="html.parser")
     title = bs.title.text
-    if not os.path.exists(f'{root}/{title}'):
-        os.mkdir(f'{root}/{title}')
+    if not os.path.exists(f'{root}/dl/{title}'):
+        os.mkdir(f'{root}/dl/{title}')
     return title
 
 def parse_page(content, page):
@@ -40,22 +41,28 @@ def parse_page(content, page):
     #     f = open(os.path.dirname(__file__)+'/tmp.html', 'r')
     #     bs = BeautifulSoup(f.read(), features="html.parser")
     #     f.close()
+    title = parse_title(content)
+    time.sleep(.5)
     for k in bs.find_all('img'):
         img = k.get('src')
         if re.search(r'\d+_?\w*\.jpg', img):
-            html = requests.get(img)
+            html = requests.get(img, timeout=8)
+            if html.status_code!=200:
+                return False
             print(img)
-            if os.path.exists(f'{root}/{title}/{page}.jpg'):
+            if os.path.exists(f'{root}/dl/{title}/{page}.jpg'):
                 return True
-            with open(f'{root}/{title}/{page}.jpg','wb') as f:
+            with open(f'{root}/dl/{title}/{page}.jpg','wb') as f:
                 f.write(html.content)
             return True
     return False
 
+# def next_charpter(url):
+
 def parse_charpter(url):
     title = parse_title(get_page(site+url))
     max_num = 0
-    for k in os.listdir(f'{root}/{title}'):
+    for k in os.listdir(f'{root}/dl/{title}'):
         res = re.search('(\d+).jpg',k)
         if res:
             max_num = max(max_num, int(res.group(1)))
@@ -65,19 +72,28 @@ def parse_charpter(url):
     # a = bs.select('a.active')[0].findNext('a')
     
 lock = threading.Lock()
-def loop_thread(url):
-    isEnd = False
-    # while !isEnd:
-    #     try:
-    #         isEnd = !down()
-    #     except EOFError:
-    #         pass
+def loop_thread(url, dls):
+    # isEnd = False
+    while True:
+        try:
+            with lock:
+                dl_id = next(dls)
+        except StopIteration:
+            break
+        print(dl_id)
+        if not down(site+url,dl_id):
+            break
 
 if __name__ == "__main__":
     # parse_charpter(comic_page)
     # parse()
-    # for k in parse_charpter(comic_page):
-    #     down
+    # threads = []
+    dls = parse_charpter(comic_page)
+    for k in range(0, 2):
+        t = threading.Thread(target=loop_thread, name=f'thread{k}', args=(comic_page, dls,))
+        t.start()
+    
+    # print('done')
     # for k in range(max_num+1,500):
     #     html = down(site+url,k)
     #     if html==None:
---
+++ video/replit/resplit.py
@@ -0,0 +1,29 @@
+import os, re
+import cv2
+import ffmpeg
+
+root = os.path.dirname(__file__)+'/'
+
+def get_video_info(input_path):
+    try:
+        probe = ffmpeg.probe(input_path)
+        video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+        if video_stream is None:
+            print('No video stream found', file=sys.stderr)
+            sys.exit(1)
+        return video_stream
+    except ffmpeg.Error as err:
+        print(str(err.stderr, encoding='utf8'))
+        sys.exit(1)
+
+def split_video(input_path):
+    # input = ffmpeg.input(input_path)
+    split = ffmpeg.input('in.mp4').filter_multi_output('split') 
+    split0 = split.stream(0) 
+    split1 = split[1] 
+    ffmpeg.concat(split0, split1).output('out.mp4').run()
+
+
+if __name__ == "__main__":
+    video_info = get_video_info(root+'./in/in.mp4')
+    print(video_info['duration'])
